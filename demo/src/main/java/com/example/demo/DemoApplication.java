abd28ed2add3e95051cc9905b2b4dafce33c380e
Narendrayadav014 <1716510067@kit.ac.in>
2021-06-06 15:09:15 +0530

    Initial Commit of Spring Project

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("This is the First Spring Project...");
+		System.out.println("Testing the first Spring Project");
+	
+	}
+
+}
